c572f11636d51117df7a55c302e6f10820fb47df
Peter <exteriorbwin@gmail.com>
2020-12-13 14:40:58 +0100

    js update

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+(function() {
+    // Mock data.
+    let todos = [
+        {title: 'Lunch', content: 'Lunch with my friends'},
+        {title: 'Lunch', content: 'Lunch with my friends'},
+        {title: 'Lunch', content: 'Lunch with my friends'}
+    ];
+    
+    const dayName = ['Sunday',
+                    'Monday',
+                    'Tuesday',
+                    'Wednesday',
+                    'Thursday',
+                    'Friday',
+                    'Satudrday',
+                ];
+                    
+    
+    
+    
+    // Parts of date.
+    const bodyDay = document.querySelector('body__day');
+    const bodyDate = document.querySelector('body__date');
+
+    // Localstorage handler object
+    const localDB = {
+        //  localDB.setItem('todos', todos);
+        setItem(key, value) {
+            value = JSON.stringify(value);
+            localStorage.setItem(key, value);
+        },
+        //  localDB.getItem('todos');
+        getItem(key) {
+            const value = localStorage.getItem(key);
+            if (!value) {
+                return null;
+            }
+            
+            return JSON.parse(value);
+
+        },
+        // localDB.removeItem('todos');
+        removeItem(key) {
+            localStorage.removeItem(key);
+        }
+    };
+
+    
+    // Initialize application.
+    const init = () => {
+        const savedTodos = localDB.getItem('todos');
+        if (savedTodos) {
+            todos = savedTodos;
+        }
+        showDate();
+    };
+
+    // Show date
+    const showDate = () => {
+        const currentDate = new Date();
+        const day = [
+            currentDate.getFullYear(),
+            currentDate.getMonth() +1,
+            currentDate.getDate()
+        
+        ].map( num => num < 10 ? `0${num}` : num );
+
+    bodyDay.textContent = dayNames[currentDate.getDay()];
+    bodyDate.textContent = day.join('-');
+};
+
+  //  console.log(localDB.getItem('todos') );
+
+
+    init();
+})();
+
+
