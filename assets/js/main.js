51a82b7603e681722c67cef9f3614cad3d29b97a
Peter <exteriorbwin@gmail.com>
2020-12-13 15:26:36 +0100

    js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,91 @@
 (function() {
-    // Mock data.
-    let todos = [
-        {title: 'Lunch', content: 'Lunch with my friends'},
-        {title: 'Lunch', content: 'Lunch with my friends'},
-        {title: 'Lunch', content: 'Lunch with my friends'}
-    ];
-    
-    const dayName = ['Sunday',
-                    'Monday',
-                    'Tuesday',
-                    'Wednesday',
-                    'Thursday',
-                    'Friday',
-                    'Satudrday',
-                ];
-                    
-    
-    
+   
+
+
+    let todos = [];
     
     // Parts of date.
-    const bodyDay = document.querySelector('body__day');
-    const bodyDate = document.querySelector('body__date');
+    const bodyDay = document.querySelector('.body__day');
+    const bodyDate = document.querySelector('.body__date');
 
-    // Localstorage handler object
+
+
+    
+    const dayNames = [
+        'Sunday', 
+        'Monday', 
+        'Tuesday', 
+        'Wednessday', 
+        'Thursday', 
+        'Friday', 
+        'Saturday',
+    ];
+
+
+
+    // Localstorage handler object.
     const localDB = {
-        //  localDB.setItem('todos', todos);
+        // localDB.setItem('todos', todos);
         setItem(key, value) {
             value = JSON.stringify(value);
             localStorage.setItem(key, value);
         },
-        //  localDB.getItem('todos');
+
+
+
+        // localDB.getItem('todos')
         getItem(key) {
             const value = localStorage.getItem(key);
             if (!value) {
                 return null;
             }
-            
-            return JSON.parse(value);
 
+            return JSON.parse(value);
         },
+
+
+
         // localDB.removeItem('todos');
         removeItem(key) {
             localStorage.removeItem(key);
         }
     };
 
-    
     // Initialize application.
     const init = () => {
+        showDate();
+        setListeners();
+        loadExistingTodos();
+    };
+
+    // Load existing todos.
+    const loadExistingTodos = () => {
         const savedTodos = localDB.getItem('todos');
         if (savedTodos) {
             todos = savedTodos;
         }
-        showDate();
+        
+        if (todos && Array.isArray(todos)) {
+            todos.forEach( todo => showTodo(todo) );
+        }
     };
 
-    // Show date
+    // Show date.
     const showDate = () => {
         const currentDate = new Date();
         const day = [
-            currentDate.getFullYear(),
-            currentDate.getMonth() +1,
-            currentDate.getDate()
-        
+            currentDate.getMonth() + 1, 
+            currentDate.getDate(),
+            currentDate.getFullYear(), 
         ].map( num => num < 10 ? `0${num}` : num );
 
-    bodyDay.textContent = dayNames[currentDate.getDay()];
-    bodyDate.textContent = day.join('-');
-};
+        bodyDay.textContent = dayNames[currentDate.getDay()];
+        bodyDate.textContent = day.join('-');
+    };
 
-  //  console.log(localDB.getItem('todos') );
-
+    
+    
+    
 
     init();
 })();
-
-
